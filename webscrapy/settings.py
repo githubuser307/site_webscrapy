21f7304de218eb98ada36ae9ba247478e786ab06
Murat <dmur_07@mail.ru>
2019-09-16 13:43:11 +0300

    Update settings.py

--- webscrapy/settings.py
+++ webscrapy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vas4if^^7f7cl#nw=4yyf(1t*_!$gm4^^+qyv#+7*vrrax^8hr'
+SECRET_KEY = '********************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
